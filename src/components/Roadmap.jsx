ea938e8b7cc9c657b520c593ed2a23c04f81e794
Khalid lawal <97804905+Iamkhalidll@users.noreply.github.com>
2025-01-03 10:30:54 +0100

    Update Roadmap.jsx

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
+
 const Roadmap = () => {
     return (
         <div key="slide1" className="grid gap-4 md:gap-8 xl:gap-20 md:items-center lg:text-3xl text-2xl">
-         <div className="md:col-span-2">
-             <h1 className="block text-3xl font-bold text-red-700 sm:text-4xl lg:text-6xl lg:leading-tight mb-3">
-                Roadmap
-              </h1>
-            <ol className="list-decimal list-inside space-y-2">
-                <li>Community Take over.</li>
-                <li>Active Community Engagement and Live Streaming.</li>
-                <li>Verification of Official X Page.</li>
-                <li>Website Development.</li>
-                <li>Dexscreener Payment.</li>
-                <li>Curve Bonding Accomplishment on Pumpfun.</li>
-                <li>Nuketown Showdown.</li>
-                <li>Dex Boosting and Trending After Curve Bonding.</li>
-                <li>Influencer Collaboration and Marketing Expansion.</li>
-                <li>Community Wallet Creation for More Marketing Engagement and Cex Exchange Listing's.</li>
-                <li>Airdrop Distribution to the First 2,000 Holders of $NUKETOWN Token.</li>
-            </ol>
+            <div className="md:col-span-2">
+                <h1 className="block text-3xl font-bold text-red-700 sm:text-4xl lg:text-6xl lg:leading-tight mb-3">
+                    Roadmap
+                </h1>
+                <ol className="list-decimal list-inside space-y-2">
+                    <li>$NUKETOWN has been CTO(Community take over) by the Solana community.</li>
+                    <li>Create an active community with live streaming content.</li>
+                    <li>Official X page verified blue check.</li>
+                    <li>Continue further website development for more updates and news for $NUKETOWN.</li>
+                    <li>Pay for Dexscreener socials update.</li>
+                    <li>Reach Curve Bonding accomplishments on Pumpfun.</li>
+                    <li>$NUKETOWN history and various versions of map versions of each Call of Duty Black Ops series.</li>
+                    <li>Onboard influencers and streamers collaborating with $NUKETOWN memecoin.</li>
+                    <li>Daily trending meeting algorithmic exposure goals for $NUKETOWN and interacting with all Call of Duty content on social media.</li>
+                    <li>Create a S.A.M. (Synaptic Algorithm Module)</li>
+                </ol>
+            </div>
         </div>
-    </div>
     );
 };
 
